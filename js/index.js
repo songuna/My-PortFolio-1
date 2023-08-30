8616e4ba28a61cc9820895352a6ee94109589c51
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-08-31 08:50:25 +0900

    .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const github = document.getElementById('gh');
 const notion = document.getElementById('nt');
+const portfolio = document.getElementById('pf');
 
 github.addEventListener("click", function () {
     window.location.href = 'https://github.com/songuna'
@@ -7,4 +8,8 @@ github.addEventListener("click", function () {
 
 notion.addEventListener("click", function () {
     window.location.href = '4knjjxfzvv@privaterelay.appleid.com'
+})
+
+portfolio.addEventListener("click", function () {
+    window.location.href = './gp.html'
 })
---
+++ js/motto.js
@@ -0,0 +1,19 @@
+const quotes = [
+    {
+        quote: "일단 시작해라. 나중에 완벽해지면 된다.",
+        author: "-론 무어-",
+    },
+    {
+        quote: "모든 사람들로부터 사랑받지 않아도 된다.",
+        author: "-니체-",
+    }
+
+];
+
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
