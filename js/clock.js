38250e191dc5d5c91e31474b2266b19b64abdf2e
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-08-31 09:39:16 +0900

    .

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,7 @@ const clock = document.querySelector("h1#clock");
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
-    const Minutes = String(date.getSeconds()).padStart(2, "0");
+    const Minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${Minutes}:${seconds}`;
 
